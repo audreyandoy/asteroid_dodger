af2fbe84fab8bbf111762c2a7aa45ba210d0a957
Audrey Andoy <audreylandoy@gmail.com>
2020-06-17 18:41:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+import random
+from pygame.locals import *
+import sys
+
+pygame.init()
+
+screen_info = pygame.display.Info()
+
+# set width and height of the screen
+screen_size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h))
+screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
+
+color = (0, 127, 255)
+
+def main():
+    clock.tick(60)
+    screen.fill(color)
+    pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
