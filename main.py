2901a9d40bbf5d19dd649d1fe9ea2d07bb271ae6
Audrey Andoy <audreylandoy@gmail.com>
2020-06-18 17:47:25 +0000

    movement for the ship in the ship class and key controls for main.py

--- main.py
+++ main.py
@@ -17,11 +17,40 @@ color = (0, 127, 255)
 # create player 
 player = Ship((20,200))
 
+# setup game variables
+NumLevels = 4
+Level = 1
+
 def main():
-    clock.tick(60)
-    screen.fill(color)
-    screen.blit(player.image, player.rect)
-    pygame.display.flip()
+    global Level, NumLevels
+
+    while Level < NumLevels:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    player.speed[0] = 10
+                if event.key == pygame.K_LEFT:
+                    player.speed[0] = -10
+                if event.key == pygame.K_UP:
+                    player.speed[1] = -10
+                if event.key == pygame.K_DOWN:
+                    player.speed[1] = 10
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    player.speed[0] = 0
+                if event.key == pygame.K_LEFT:
+                    player.speed[0] = 0
+                if event.key == pygame.K_UP:
+                    player.speed[1] = 0
+                if event.key == pygame.K_DOWN:
+                    player.speed[1] = 0
+
+        # draw sprites and add colors to screen, UI
+        player.update()
+        screen.fill(color)
+        screen.blit(player.image, player.rect)
+        pygame.display.flip()
 
 if __name__ == '__main__':
     main()
