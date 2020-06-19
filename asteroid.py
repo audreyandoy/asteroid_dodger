fecce67a569770b17a70ee0917a41ab00a370878
Audrey Andoy <audreylandoy@gmail.com>
2020-06-19 17:24:18 +0000

    adding collision and win screen

---
+++ asteroid.py
@@ -0,0 +1,31 @@
+import pygame, random
+
+class Asteroid(pygame.sprite.Sprite):
+    def __init__(self, pos, scale):
+        super().__init__()
+        self.image = pygame.image.load("asteroid.png")
+        self.scale = scale
+        width = int(self.image.get_rect().width*self.scale)
+        height = int(self.image.get_rect().height*self.scale)
+        self.image = pygame.transform.smoothscale(
+            self.image, (width, height)
+        )
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+        self.speed = pygame.math.Vector2(0,3)
+        self.speed.rotate_ip(random.randint(0, 360))
+
+
+    def update(self):
+        self.rect.move_ip(self.speed)
+        screen_info = pygame.display.Info()
+        if self.rect.left < 0 or self.rect.right > screen_info.current_w:
+            self.speed[0] *= -1
+            self.rect.move_ip((self.speed[0],0))
+            self.image = pygame.transform.flip(self.image, True, False)
+        if self.rect.top < 0 or self.rect.bottom > screen_info.current_h:
+            self.speed[1] *= -1
+            self.rect.move_ip((0, self.speed[1]))
+            self.image = pygame.transform.flip(self.image, True, False)
+
+  
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from pygame.locals import *
 import sys
 from ship import Ship
+from asteroid import Asteroid
 
 pygame.init()
 
@@ -21,8 +22,43 @@ player = Ship((20,200))
 NumLevels = 4
 Level = 1
 
+asteroids = pygame.sprite.Group()
+asteroid_count = 3
+
+def init():
+    global asteroids, asteroid_count
+    player.reset((20, 200))
+    asteroids.empty()
+    asteroid_count += 3
+    for i in range(asteroid_count):
+        asteroids.add(
+            Asteroid(
+                # pos (x,y)
+                (
+                    random.randint(50, width-50),
+                    random.randint(50, height-50)
+                ),
+                #scale
+                random.choice([0.07, 0.08, 0.09])
+            )
+        )
+
+def win():
+    font = pygame.font.SysFont(None, 70)
+    text = font.render("You escaped!!", True, (255, 0, 0))
+    text_rect = text.get_rect()
+    text_rect.center = (width // 2, height // 2)
+    while True:
+        clock.tick(60)
+        screen.fill(color)
+        screen.blit(text, text_rect)
+        pygame.display.flip()
+
+
 def main():
-    global Level, NumLevels
+    global Level, NumLevels, asteroids
+
+    init()
 
     while Level < NumLevels:
         clock.tick(60)
@@ -46,11 +82,25 @@ def main():
                 if event.key == pygame.K_DOWN:
                     player.speed[1] = 0
 
+        # check for collision 
+        player_hit = pygame.sprite.spritecollide(player, asteroids, False)
+
+        if player.checkReset(width):
+            Level += 1
+            init()
+            print(Level)
+        elif player_hit:
+            player.reset((20, 200))
+        
         # draw sprites and add colors to screen, UI
         player.update()
+        asteroids.update()
         screen.fill(color)
+        asteroids.draw(screen)
         screen.blit(player.image, player.rect)
         pygame.display.flip()
+
+    win()
 
 if __name__ == '__main__':
     main()
